c22b0e9fdcec8150bb3571e98aa05db54dcd5ee9
insane4l <43768644+insane4l@users.noreply.github.com>
2021-11-01 22:42:40 +0200

    Tabs module created (+some styles)

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,49 @@
+const tabs = () => {
+
+    function bindTabs(headerSelector, tabSelector, contentSelector, activeClass) {
+        const header = document.querySelector(headerSelector),
+              tabs = header.querySelectorAll(tabSelector),
+              content = document.querySelectorAll(contentSelector);
+        
+        function hideAllTabContent() {
+            tabs.forEach( (tab, i) => {
+                tab.classList.remove(activeClass);
+                content[i].style.display = 'none';
+            })
+        };
+
+        function showTabContent(i = 0) {
+            tabs[i].classList.add(activeClass);
+            content[i].style.display = 'block';
+        };
+
+        function checkTarget(target) {
+            const className = tabSelector.replace(/\./, '');
+            return target && ( target.classList.contains(className) || target.parentNode.classList.contains(className) );
+        };
+
+        header.addEventListener('click', (e) => {
+            const target = e.target;
+            const isRightTarget = checkTarget(target);
+
+            if (!isRightTarget) return
+            
+            tabs.forEach( (el, i) => {
+                if (el === target || el === target.parentNode) {
+                    hideAllTabContent();
+                    showTabContent(i);
+                }
+            });
+            
+        });
+
+        hideAllTabContent();
+        showTabContent();
+    }
+
+    
+    bindTabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    bindTabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+}
+
+export default tabs;
